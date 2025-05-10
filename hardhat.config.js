09e8c400cbdc825f45eb77e8ae76282b247e1f31
vPere <pere.vidiella@gmail.com>
2025-05-10 18:50:45 +0200

    Updates config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: "http://127.0.0.1:8545", // This is for local Hardhat network
+    },
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/9nMjQEfnED5wUjFHqt7_7PCIz8ofRDN5", // Use your Alchemy API key here
+      },
     },
   },
 };
