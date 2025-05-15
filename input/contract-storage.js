73c1a96f25c441be1535aa91812c0594ac048a39
vPere <pere.vidiella@gmail.com>
2025-05-15 12:33:00 +0200

    IAFA zero addr

--- input/contract-storage.js
+++ input/contract-storage.js
@@ -3148,6 +3148,8 @@ const contractAddresses = [
 
 const contractAddresses = [
     '0xc3f733ca98e0dad0386979eb96fb1722a1a05e69',
+    '0x32e136be91606c645ff62ff7075b2f0472fa13e1',
+    '0x5536b6aadd29eaf0db112bb28046a5fad3761bd4'
     ];
 function getContractAddresses() {
     return contractAddresses;
